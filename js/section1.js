29b767d9dce8d36b507c63b5d242f47b7304f962
OjByczku2137 <71936958+OjByczku2137@users.noreply.github.com>
2021-10-19 22:51:47 +0200

    dsada
    sda

--- js/section1.js
+++ js/section1.js
@@ -3,7 +3,6 @@ let planet1 = document.getElementById('planet1');
 let planet2 = document.getElementById('planet2');
 let planet3 = document.getElementById('planet3');
 let button = document.getElementById('button');
-let satelite = document.getElementById('satelite');
 let astronaut1 = document.getElementById('astronaut1');
 let header = document.getElementById('header');
 
@@ -14,8 +13,6 @@ window.addEventListener('scroll', function(){
   planet1.style.top = 0 + scrollpos * -.1 + '%';
   planet2.style.top = 0 + scrollpos * -.05 + '%';
   planet3.style.top = 0 + scrollpos * -.1 + '%';
-  satelite.style.top = 0 + scrollpos * -.03 + '%';
-  satelite.style.left = 0 + scrollpos * .01 + '%';
   astronaut1.style.top = 0 + scrollpos * -.02 + '%';
   astronaut1.style.left = 0 + scrollpos * -.02 + '%';
 })
