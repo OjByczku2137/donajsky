0dddd7de9c58a392d31dfc6d92a902dccb2f05fe
OjByczku2137 <71936958+OjByczku2137@users.noreply.github.com>
2021-11-09 08:19:26 +0100

    xcxvbfgnfbdfsa

--- js/section2.js
+++ js/section2.js
@@ -5,23 +5,23 @@ let astronaut2 = document.getElementById('astronaut2');
 const number = 0.09;
 window.addEventListener('scroll', function(){
     let scrollpos = window.scrollY;
-    if (scrollpos < 3400){
+    if (scrollpos < 3350){
     if (scrollpos > 1800){
         rocket.style.top = 137 + scrollpos * number + '%';
     }
-    if (scrollpos > 3300){
+    if (scrollpos > 3290){
         duzydym.style.opacity = 1;
     }
     else{
         duzydym.style.opacity = 0;
     }
-    if (scrollpos >= 3350){
+    if (scrollpos >= 3330){
         flag.style.opacity = 1;
     }
     else{
         flag.style.opacity = 0;
     }
-    if (scrollpos >= 3350){
+    if (scrollpos >= 3330){
         astronaut2.style.opacity = 1;
     }
     else{
