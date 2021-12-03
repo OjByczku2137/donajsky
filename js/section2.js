c53490c78f2836ef06d8dd626d5c9b1f586acef8
OjByczku2137 <71936958+OjByczku2137@users.noreply.github.com>
2021-12-03 22:33:12 +0100

    dzfgh

--- js/section2.js
+++ js/section2.js
@@ -1,15 +1,15 @@
-let rocket = document.getElementById('rocket');
-let duzydym = document.getElementById('bigsmoke');
-let flag = document.getElementById('flag');
-let astronaut2 = document.getElementById('astronaut2');
-const number = 0.09;
+var rocket = document.getElementById('rocket');
+var duzydym = document.getElementById('bigsmoke');
+var flag = document.getElementById('flag');
+var astronaut2 = document.getElementById('astronaut2');
+var number = 0.09;
 window.addEventListener('scroll', function(){
-    let scrollpos = window.scrollY;
+    var scrollpos = window.scrollY;
     if (scrollpos < 3350){
     if (scrollpos > 1800){
         rocket.style.top = 137 + scrollpos * number + '%';
     }
-    if (scrollpos > 3290){
+    if (scrollpos > 3330){
         duzydym.style.opacity = 1;
     }
     else{
@@ -28,4 +28,17 @@ window.addEventListener('scroll', function(){
        astronaut2.style.opacity = 0;
     }}
     
+});
+
+
+window.addEventListener('scroll', function(){
+    var scroll = window.scrollY;
+    var rakieta = document.getElementById('animation');
+    var dym = document.getElementById('animation-smoke');
+    if(scroll > 2050){
+        dym.classList.add("dym");
+        rakieta.classList.add("animacja");
+    }
+
+        
 });
